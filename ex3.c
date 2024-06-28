0e8b51e38baeb049e48049f954ff8387415f7a2b
nines <127805872+nines75@users.noreply.github.com>
2024-06-28 12:39:43 +0900

    勝ち負け表示追加

--- ex3.c
+++ ex3.c
@@ -16,6 +16,12 @@ int main() {
     printf("Dice 1: %d\n", a);
     printf("Dice 2: %d\n", b);
     printf("Total value : %d\n", a+b);
+    
+    if (a + b > 7) {
+        printf("You won\n");
+    } else {
+        printf("You lost\n");
+    }
 }
 
 int GetRandom(int min, int max) {
