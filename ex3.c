dc3ea05fd9f98a96ae252d79ca79658776ecb6e2
nines <127805872+nines75@users.noreply.github.com>
2024-06-28 12:27:33 +0900

    さいころゲーム：最初の見直し

---
+++ ex3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int GetRandom(int min, int max);
+
+int main() {
+    int a, b;
+
+    printf("Rolling dice...\n");
+
+    srand((unsigned int)time(NULL));
+    a = GetRandom(1, 6);
+    b = GetRandom(1, 6);
+
+    printf("Dice 1: %d\n", a);
+    printf("Dice 2: %d\n", b);
+    printf("Total value : %d\n", a+b);
+}
+
+int GetRandom(int min, int max) {
+    return min + (int)(rand() * (max - min + 1.0) / (1.0 + RAND_MAX));
+}
